a51ef70f4e924408ef53e64b0839b741effa6209
Your Name <114593318+Manuelyjc@users.noreply.github.com>
2024-05-24 09:34:45 -0500

    agrego el formulario para subir la cancion

---
+++ spoty/src/app/View_Admin/components/Vista_admin.tsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import React from 'react';
+
+function Vista_admin() {
+  return (
+    <div className=''>
+      <form>
+        <div className="space-y-12">
+         
+       
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">Formulario para subir cancion </h2>
+            <p>porfavor ingrese todos los campos, para poder subir el album, correctamente</p>
+            <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-4">
+                <label htmlFor="username" className=" text-sm font-medium leading-6 text-gray-900">Ingresa el nombre del artista</label>
+                <div className="mt-2">
+              <input
+              id='artista'
+                name="artista"
+                type="text"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <label htmlFor="username" className=" text-sm font-medium leading-6 text-gray-900">Ingresa el nombre de la cancion</label>
+                <div className="mt-2">
+              <input
+              id='cancion'
+                name="cancion"
+                type="text"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+              </div>
+
+              <div className="col-span-full">
+                <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">Ingrese una description para el album </label>
+                <div className="mt-2">
+                  <textarea
+                    id="about"
+                    name="about"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  ></textarea>
+                </div>
+              </div>
+              <div className="col-span-full">
+                <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">Aqui puedes subir el album</label>
+                <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                  <div className="text-center">
+                    <svg className="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clipRule="evenodd" />
+                    </svg>
+                    <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                      <label htmlFor="file-upload" className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+                        <span>Subir album</span>
+                        <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Vista_admin;
---
+++ spoty/src/app/View_Admin/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Vista_admin from "../View_Admin/components/Vista_admin";
+
+
+function page() {
+  return (
+    <div>
+        <Vista_admin/>
+    </div>
+  )
+}
+
+export default page
